bd46b744e80c0ed398a0f5094bd86d749d11a06c
KushajveerSingh <kushajreal@gmail.com>
2022-11-05 18:53:54 -0400

    setup chat api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer } from './components';
+
+const API_KEY = 'bqdssx42sjae';
+const client = StreamChat.getInstance(API_KEY);
 
 const App = () => {
   return (
-    <div>
-      <h1>UFC Chat App</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team dark">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
 };
---
+++ client/src/components/ChannelContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChannelContainer = () => {
+  return (
+    <div>
+      <span>ChannelContainer</span>
+    </div>
+  );
+};
+
+export default ChannelContainer;
