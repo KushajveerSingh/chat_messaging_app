0f44c3a88dc21bc4e83ad0dfee94930c2dc4b4fe
KushajveerSingh <kushajreal@gmail.com>
2022-11-06 00:19:45 -0400

    create signin/signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,18 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-import { ChannelListContainer, ChannelContainer } from './components';
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
 import './App.css';
 
 const client = StreamChat.getInstance(process.env.REACT_APP_STREAM_KEY);
 
+const authToken = false;
+
 const App = () => {
+  if (!authToken) {
+    return <Auth />;
+  }
+
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
